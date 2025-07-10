f7def461abac4a9423160790bb0732146cb69e0f
Wyatt James <wyattsamueljames@gmail.com>
2025-07-10 10:41:53 -0600

    feat: add sort n spread

--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+from typing import Callable
+
+NUM_KEY: str = "num"
+
+
 def get_word_count(text: str) -> int:
     return len(text.split())
 
@@ -13,5 +18,24 @@ def get_char_count(text: str) -> dict[str, int]:
     return char_count_dict
 
 
-def sort_on_int_val[P](int_val_dict: dict[P, int], key: P) -> int:
-    return int_val_dict[key]
+type ValidValue = str | int
+
+
+def sort_on_int_val(int_val_dict: dict[str, ValidValue]) -> ValidValue:
+    return int_val_dict[NUM_KEY]
+
+
+def spread_dict(dictToSpread: dict[str, ValidValue]) -> list[dict[str, ValidValue]]:
+    spread_dicts: list[dict[str, ValidValue]] = []
+    for k, v in dictToSpread.items():
+        spread_dicts.append({"char": k, NUM_KEY: v})
+    return spread_dicts
+
+
+def sort_n_spread(
+    dictionary: dict[str, ValidValue],
+    sort_on: Callable[[dict[str, ValidValue]], ValidValue],
+) -> list[dict[str, ValidValue]]:
+    dicts: list[dict[str, ValidValue]] = spread_dict(dictionary)
+    dicts.sort(reverse=True, key=sort_on)
+    return dicts
