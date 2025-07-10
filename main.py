f33edb74fb415341c99d2a7b8fb7b637769598d0
Wyatt James <wyattsamueljames@gmail.com>
2025-07-10 09:52:54 -0600

    feat: add get book text

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_test(book_path: str) -> str:
+    with open(book_path, "r") as book:
+        return book.read()
+
+
+def main() -> None:
+    book_text: str = get_book_test("books/frankenstein.txt")
+    print(book_text)
+
+
+if __name__ == "__main__":
+    main()
